2f9be4edd9381c5852c90240a547aabb4c49f172
David <davidmunyiri2@gmail.com>
2023-11-27 15:36:23 +0300

    Update settings.py

--- movie_project/settings.py
+++ movie_project/settings.py
@@ -100,7 +100,11 @@ AUTH_PASSWORD_VALIDATORS = [
         'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
     },
 ]
-
+# CACHE
+CACHES = { "default": {
+     "BACKEND": "django.core.cache.backends.locmem.LocMemCache", 
+     "LOCATION": "unique-movie",
+ } }
 
 # Internationalization
 # https://docs.djangoproject.com/en/4.2/topics/i18n/
