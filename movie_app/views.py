4c670177ea705264d35cb768ec55f81338acdade
David <davidmunyiri2@gmail.com>
2023-11-27 13:39:38 +0300

    created a seperate function for calling the api tmdb

--- movie_app/views.py
+++ movie_app/views.py
@@ -1,4 +1,5 @@
 import json
+import logging
 from django.shortcuts import render
 from django.core.paginator import Paginator, PageNotAnInteger, EmptyPage
 from tmdbv3api import TMDb
@@ -6,31 +7,95 @@ from tmdbv3api import Movie
 import requests
 
 
+logger = logging.getLogger(__name__)
+
+def fetch_tmdb_data(page, url):
+    """
+    fetch data from The movie Db
+    """
+    url = url
+    headers = {
+        "accept": "application/json",
+        "Authorization": "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI1ZDVlNDlkNzQyNDVkNGZiNzg1YTc0YjY1NzI3ZWZkNCIsInN1YiI6IjY1NTQ3Y2I3NTM4NjZlMDBhYmFhYTAwYSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.uNLKtu3ip1TMnNrN5RPKkEYcDJXI-X2d_qrp_AdGNoE"
+    }
+
+    params = {
+        'page': page,
+    }
+
+    try:
+        response = requests.get(url, headers=headers, params=params)
+        response.raise_for_status()  # Raise HTTPError for bad responses
+        movie_data = response.json()
+        return movie_data.get('results', [])
+    except requests.exceptions.RequestException as e:
+        # Handle exception (e.g., log the error, provide a default response)
+        logger.error(f"Error fetching TMDb data: {e}")
+        return []
+    
 def latest_movies(request):
     """
-    Function for getting latest movies 
+    Function for dispaying the latest movies 
     """
-    movies = None
-    pages = 100
-    for page in range(1,pages + 1):
-        url = "https://api.themoviedb.org/3/movie/now_playing"
-        print(f'page is {page}')
+    url = "https://api.themoviedb.org/3/movie/now_playing"
+    movies = []
 
-        headers = {
-            "accept": "application/json",
-            "Authorization": "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI1ZDVlNDlkNzQyNDVkNGZiNzg1YTc0YjY1NzI3ZWZkNCIsInN1YiI6IjY1NTQ3Y2I3NTM4NjZlMDBhYmFhYTAwYSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.uNLKtu3ip1TMnNrN5RPKkEYcDJXI-X2d_qrp_AdGNoE"
-        }
+    # Fetch TMDb data dynamically until no more results
+    page = 1
+    while True:
+        logger.debug(f'Fetching page {page} from TMDb API')
+        page_data = fetch_tmdb_data(page, url)
 
-        params = {
-            'page': page,
-        }
+        if not page_data:
+            # No more results, break out of the loop
+            break
 
-        response = requests.get(url, headers=headers, params=params)
-        movie_data = json.loads(response.content)
-        if movies == None:
-            movies = movie_data.get('results', [])
-        else:
-            movies.extend(movie_data.get('results'))
+        movies.extend(page_data)
+        page += 1
+
+    # Display movies per page
+    movies_per_page = 36
+
+    paginator = Paginator(movies, movies_per_page)
+
+    page_number = request.GET.get('page')
+    try:
+        current_page = paginator.get_page(page_number)
+    except PageNotAnInteger:
+        # If the page parameter is not an integer, deliver the first page
+        current_page = paginator.get_page(1)
+    except EmptyPage:
+        # If the page is out of range (e.g., 9999), deliver the last page
+        current_page = paginator.get_page(paginator.num_pages)
+
+    context = {'latest_movie': current_page}
+    return render(request, 'movie_app/index.html', context)    
+
+# def latest_movies(request):
+#     """
+#     Function for getting latest movies 
+#     """
+#     movies = None
+#     pages = 100
+#     for page in range(1,pages + 1):
+#         url = "https://api.themoviedb.org/3/movie/now_playing"
+#         print(f'page is {page}')
+
+#         headers = {
+#             "accept": "application/json",
+#             "Authorization": "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI1ZDVlNDlkNzQyNDVkNGZiNzg1YTc0YjY1NzI3ZWZkNCIsInN1YiI6IjY1NTQ3Y2I3NTM4NjZlMDBhYmFhYTAwYSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.uNLKtu3ip1TMnNrN5RPKkEYcDJXI-X2d_qrp_AdGNoE"
+#         }
+
+#         params = {
+#             'page': page,
+#         }
+
+#         response = requests.get(url, headers=headers, params=params)
+#         movie_data = json.loads(response.content)
+#         if movies == None:
+#             movies = movie_data.get('results', [])
+#         else:
+#             movies.extend(movie_data.get('results'))
 
 
     # Dispaly movies per page
