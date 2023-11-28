aa8fefd84eee0ba68fffee06322ad9687a5fd57f
David <davidmunyiri2@gmail.com>
2023-11-28 17:06:18 +0300

    Update tests.py

--- movie_app/tests.py
+++ movie_app/tests.py
@@ -1,10 +1,14 @@
 import unittest
 from unittest.mock import patch, MagicMock
-from movie_project.movie_app.views import fetch_tmdb_data
+
+import requests
+from movie_app.views import fetch_tmdb_data
+
+bearer_token = "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI1ZDVlNDlkNzQyNDVkNGZiNzg1YTc0YjY1NzI3ZWZkNCIsInN1YiI6IjY1NTQ3Y2I3NTM4NjZlMDBhYmFhYTAwYSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.uNLKtu3ip1TMnNrN5RPKkEYcDJXI-X2d_qrp_AdGNoE"
 
 class TestFetchTmdbData(unittest.TestCase):
 
-    @patch('movie_project.movie_app.requests.get')
+    @patch('movie_app.views.requests.get')
     def test_fetch_tmdb_data_with_results(self, mock_get):
         # Mocking the requests.get function
         mock_response = MagicMock()
@@ -17,9 +21,9 @@ class TestFetchTmdbData(unittest.TestCase):
 
         # Assertions
         self.assertEqual(result, [{'title': 'Movie 1'}, {'title': 'Movie 2'}])
-        mock_get.assert_called_once_with('mock_url', headers=..., params={'page': 1})
+        mock_get.assert_called_once_with('mock_url', headers={'accept': 'application/json', 'Authorization': bearer_token}, params={'page': 1})
 
-    @patch('movie_project.movie_app.requests.get')
+    @patch('movie_app.views.requests.get')
     def test_fetch_tmdb_data_with_movie_id(self, mock_get):
         # Mocking the requests.get function
         mock_response = MagicMock()
@@ -32,9 +36,9 @@ class TestFetchTmdbData(unittest.TestCase):
 
         # Assertions
         self.assertEqual(result, {'title': 'Mock Movie'})
-        mock_get.assert_called_once_with('mock_url', headers=...)
+        mock_get.assert_called_once_with('mock_url', headers={'accept': 'application/json', 'Authorization': bearer_token})
 
-    @patch('movie_project.movie_app.requests.get')
+    @patch('movie_app.views.requests.get')
     def test_fetch_tmdb_data_with_request_exception(self, mock_get):
         # Mocking the requests.get function to raise an exception
         mock_get.side_effect = requests.exceptions.RequestException('Mock Request Exception')
